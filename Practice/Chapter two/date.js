6e5870ca48495937d4c3cc6c1ad4e379d945fab6
Tawsif05 <tawsifmahmud05@gmail.com>
2022-01-30 00:06:25 +0600

    chapter one completed

---
+++ Practice/Chapter two/date.js
@@ -0,0 +1,13 @@
+var date = new Date()
+console.log(date.toDateString())
+console.log(date.toTimeString())
+console.log(date.toLocaleDateString())
+
+console.log(date.getFullYear())
+console.log(date.getMonth())
+console.log(date.getDate())
+
+
+
+
+
---
+++ Practice/Chapter two/math.js
@@ -0,0 +1,8 @@
+console.log(Math.E)
+console.log(Math.PI)
+var a = 3.456
+console.log(Math.abs(a))
+console.log(Math.floor(a))
+console.log(Math.ceil(a))
+
+
